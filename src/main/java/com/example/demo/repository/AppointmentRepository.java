41953dacfbcee4aa5e3780f314c96bca6c5f6cfb
Osama-Sammar <osamasammar58@gmail.com>
2023-05-22 14:11:15 +0300

    Revert "v1"
    This reverts commit f6f0bc4ebb957a974653d710682e2acb8b9a5ea3.

--- src/main/java/com/example/demo/repository/AppointmentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.entity.Appointment;
-import com.example.demo.entity.Doctor;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AppointmentRepository  extends JpaRepository<Appointment, Long>{
-}
