0f5b670819e94edf4c9b23bba1d258b365737261
Osama-Sammar <osamasammar58@gmail.com>
2023-05-22 14:15:45 +0300

    Revert "Revert "Revert "v1"""
    This reverts commit 7b072d49dd7b2b79950c0c368b7044f32d9e4241.

--- src/main/java/com/example/demo/repository/HospitalStaffRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.entity.Doctor;
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.demo.entity.HospitalStaff;
-
-public interface HospitalStaffRepository extends JpaRepository<HospitalStaff, Long> {
-
-}
